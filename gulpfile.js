79d6f5f2a2557e606270c0fed0ad66ca5d7f3323
Ihor666777 <i-pylypyak@ukr.net>
2017-11-23 13:27:54 +0200

    finished task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
-'use strict'
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var gulpSequence = require('gulp-sequence');
 
-const gulp = require('gulp');
-const connect = require('gulp-connect');
 
- 
-gulp.task('connect', function() {
-  connect.server();
-});
+//livereload task
+gulp.task('livereload', function(){
+  browserSync.init({
+    server: 'src/'
+  })
+  gulp.watch('src/*.html');
+  gulp.watch("src/scripts/*.js");
+  gulp.watch("src/styles/*.css");
+  gulp.watch("src/scripts/*.js").on('change', browserSync.reload);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch('src/styles/*.css').on('change', browserSync.reload);
+})
+
+gulp.task('default', ['livereload']);
+
